aca3b8fe0e8ced18833365399a9e6aeca72d4cce
gorm <gormleyethan@gmail.com>
2023-11-02 13:50:58 -0700

    nmap working

--- utils/getNmapInfo.py
+++ utils/getNmapInfo.py
@@ -1,5 +1,17 @@
 import nmap
+import logging
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+from zeroconf import ServiceBrowser, Zeroconf
 
+# Setup logging
+log_file_handler = logging.FileHandler('summary.log', mode='w')
+log_file_handler.setLevel(logging.INFO)
+log_file_handler.setFormatter(logging.Formatter('%(levelname)s:%(name)s: %(message)s'))
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(log_file_handler)
 # Dictionary mapping printer languages to common manufacturers
 printer_languages = {
     'PJL': ['HP', 'Lexmark', 'Canon'],
@@ -9,22 +21,36 @@ printer_languages = {
     'IPP': ['HP', 'Canon', 'Epson', 'Brother', 'Lexmark']
 }
 
+class MyListener:
+    def remove_service(self, zeroconf, type, name):
+        logging.info(f"Service {name} removed")
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        logging.info(f"Service {name} added, service info: {info}")
+
 def grab_banner(ip_address, port):
     nm = nmap.PortScanner()
-    nm.scan(ip_address, arguments=f'-p {port} --script=banner')
     try:
-        banner = nm.csv().split('\n')[1].split(';')[4]
-        return banner if banner else None
-    except IndexError:
-        print(f"No banner found on {ip_address}:{port}")
+        nm.scan(ip_address, arguments=f'-p {port} -sV --script=default --max-retries 1 --host-timeout 30s')
+        service_info = nm[ip_address].get('tcp', {}).get(port, {})
+        if service_info:
+            return f"{service_info.get('name', 'Unknown')} ({service_info.get('product', 'Unknown')})"
+        else:
+            logging.info(f"No service information found on {ip_address}:{port}")
+            return None
+    except nmap.PortScannerError as e:
+        logging.error(f"PortScannerError on {ip_address}:{port} - {str(e)}")
+        return None
+    except KeyError:
+        # Ignore KeyError exceptions
         return None
 
 def analyze_banner(banner):
-# Check if banner contains common printer-related terms
     printer_keywords = ['printer', 'ipp', 'pcl', 'laserjet', 'deskjet', 'epson', 'xerox', 'brother', 'lexmark', 'canon']
     for keyword in printer_keywords:
         if keyword.lower() in banner.lower():
-            return keyword  # Return the matching keyword
+            return keyword
     return None
 
 def get_printer_languages(manufacturer):
@@ -34,23 +60,42 @@ def get_printer_languages(manufacturer):
             languages.append(language)
     return languages
 
-def scan_for_printers(ip_list):
+def scan_ip(ip_address):
     printer_ports = [9100, 515, 5357, 631]
-    for ip_address in ip_list:
-        for port in printer_ports:
-            banner = grab_banner(ip_address, port)
-            if banner:
-                print(f"Banner on {ip_address}:{port}:\n{banner}")
-                manufacturer_keyword = analyze_banner(banner)
-                if manufacturer_keyword:
-                    print(f"The device at {ip_address}:{port} is likely a printer.")
-                    languages = get_printer_languages(manufacturer_keyword)
-                    if languages:
-                        print(f"Common printer languages used by {manufacturer_keyword}: {', '.join(languages)}\n")
-                    else:
-                        print(f"No common printer languages found for {manufacturer_keyword}.\n")
+    result_data = {'ip_address': ip_address, 'printers': []}
+    for port in printer_ports:
+        banner = grab_banner(ip_address, port)
+        if banner:
+            printer_data = {
+                'port': port,
+                'banner': banner,
+            }
+            manufacturer_keyword = analyze_banner(banner)
+            if manufacturer_keyword:
+                printer_data['manufacturer'] = manufacturer_keyword
+                languages = get_printer_languages(manufacturer_keyword)
+                if languages:
+                    printer_data['languages'] = languages
+                else:
+                    printer_data['languages'] = 'Unknown'
+            result_data['printers'].append(printer_data)
+    # Only log the result if printers were found
+    if result_data['printers']:
+        logging.info(result_data)
+
+def print_summary():
+    with open('summary.log', 'r') as log_file:
+        log_content = log_file.read()
+    print(log_content)
 
 if __name__ == "__main__":
-    # Assume you have a list of IP addresses to scan
-    ip_list = ['192.168.1.35']
-    scan_for_printers(ip_list)
+    zeroconf = Zeroconf()
+    listener = MyListener()
+    browser = ServiceBrowser(zeroconf, "_ipp._tcp.local.", listener)
+    try:
+        ip_list = ['192.168.1.' + str(i) for i in range(1, 40)]
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(scan_ip, ip_list)
+    finally:
+        zeroconf.close()
+    print_summary()
